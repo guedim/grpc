069c642a7284869790e4c066fc21704f71f263a1
Mario Guerrero <mario.guerrero@payulatam.com>
2021-05-16 00:04:12 -0500

    fix project structure

--- grpc-java/src/main/java/com/guedim/model/protobf/PersonCompositionDemo.java
+++ grpc-java/src/main/java/com/guedim/model/protobf/PersonCompositionDemo.java
@@ -1,2 +1,31 @@
-package com.guedim.model.protobf;public class PersonCompositionDemo {
+package com.guedim.model.protobf;
+
+import com.guedim.model.Address;
+import com.guedim.model.Car;
+import com.guedim.model.Person;
+
+public class PersonCompositionDemo {
+
+    public static void main(String[] args) {
+
+        Address address = Address.newBuilder()
+                .setPostbox(123)
+                .setStreet("22")
+                .setCity("Bogotá")
+                .build();
+
+        Car car = Car.newBuilder()
+                .setMake("Renailt")
+                .setModel("Clio")
+                .setYear(2015)
+                .build();
+
+        Person person = Person.newBuilder()
+                .setName("Matías")
+                .setAge(4)
+                .setCar(car)
+                .setAddress(address)
+                .build();
+
+    }
 }
