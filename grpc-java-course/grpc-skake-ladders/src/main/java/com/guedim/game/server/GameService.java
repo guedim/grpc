b3e461a99aaa7eed2b575d601d5339466bc95547
Mario Guerrero <mario.guerrero@payulatam.com>
2021-05-20 01:13:57 -0500

    snake and ladders structure

--- grpc-java-course/grpc-skake-ladders/src/main/java/com/guedim/game/server/GameService.java
+++ grpc-java-course/grpc-skake-ladders/src/main/java/com/guedim/game/server/GameService.java
@@ -1,2 +1,23 @@
-package com.guedim.game.server;public class GameService {
+package com.guedim.game.server;
+
+import com.guedim.game.Die;
+import com.guedim.game.GameServiceGrpc;
+import com.guedim.game.GameState;
+import com.guedim.game.Player;
+import io.grpc.stub.StreamObserver;
+
+public class GameService extends GameServiceGrpc.GameServiceImplBase {
+    @Override
+    public StreamObserver<Die> roll(StreamObserver<GameState> responseObserver) {
+
+        Player client = Player.newBuilder()
+                .setName("client")
+                .setPosition(0)
+                .build();
+        Player server = Player.newBuilder()
+                .setName("server")
+                .setPosition(0)
+                .build();
+        return new DieStramingRequest(client, server, responseObserver);
+    }
 }
