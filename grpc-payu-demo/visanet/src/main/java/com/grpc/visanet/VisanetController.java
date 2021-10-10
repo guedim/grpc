1c5cdabfed971df41193bd1be24b9b170c6a1fc6
Mario Guerrero <mario.guerrero@payulatam.com>
2021-10-10 01:18:13 -0500

    visanet grpc client

--- grpc-payu-demo/visanet/src/main/java/com/grpc/visanet/VisanetController.java
+++ grpc-payu-demo/visanet/src/main/java/com/grpc/visanet/VisanetController.java
@@ -1,6 +1,8 @@
 package com.grpc.visanet;
 
 import com.grpc.core.model.*;
+import com.grpc.visanet.service.ResponseCodeClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VisanetController {
 
+    private ResponseCodeClientService service;
+
+    @Autowired
+    public VisanetController(ResponseCodeClientService service) {
+        this.service = service;
+    }
+
     @PostMapping(value = "/visanet-payment")
     PaymentResponse process(@RequestBody Request request) {
-        PaymentResponse response = new PaymentResponse();
-        response.setState(PayUState.APPROVED);
-        response.setResponseCode(PayUResponseCode.APPROVED);
-        return response;
+
+        //TODO: Generar un String Aleaotiro
+        String paymentNetworkResponse = "00";
+
+        return service.getResponse(paymentNetworkResponse);
     }
 }
