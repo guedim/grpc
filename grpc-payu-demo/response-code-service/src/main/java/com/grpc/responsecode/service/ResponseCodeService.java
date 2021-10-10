a10478fc98332738530e2de20319687b9da2338d
Mario Guerrero <mario.guerrero@payulatam.com>
2021-10-10 00:14:49 -0500

    fiist grpc service version

--- grpc-payu-demo/response-code-service/src/main/java/com/grpc/responsecode/service/ResponseCodeService.java
+++ grpc-payu-demo/response-code-service/src/main/java/com/grpc/responsecode/service/ResponseCodeService.java
@@ -1,9 +1,31 @@
 package com.grpc.responsecode.service;
 
 
+import com.grpc.payu.common.PayUPaymentNetwork;
+import com.grpc.payu.common.PayUResponseCode;
+import com.grpc.payu.common.PayUState;
+import com.grpc.payu.response.ResponseCodeRequest;
+import com.grpc.payu.response.ResponseCodeResponse;
 import com.grpc.payu.response.ResponseCodeServiceGrpc;
+import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 
 @GRpcService
 public class ResponseCodeService extends ResponseCodeServiceGrpc.ResponseCodeServiceImplBase {
+
+    @Override
+    public void getResponseCode(ResponseCodeRequest request, StreamObserver<ResponseCodeResponse> responseObserver) {
+
+        PayUPaymentNetwork payUPaymentNetwork =  request.getPayuPaymentNetwork();
+        String externalResponseCode = request.getExternalPaymentNetworkResponseCode();
+
+
+        ResponseCodeResponse response = ResponseCodeResponse.newBuilder()
+                .setPayuReponseCode(PayUResponseCode.EXCEEDED_AMOUNT)
+                .setPayuState(PayUState.DECLINED)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
