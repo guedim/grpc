9de542fc9ea56193b8db210dbb8b826567f031f3
Mario Guerrero <mario.guerrero@payulatam.com>
2021-10-13 10:09:21 -0500

    tracing

--- grpc-payu-demo/credibanco/src/main/java/com/grpc/credibanco/CredibanCo.java
+++ grpc-payu-demo/credibanco/src/main/java/com/grpc/credibanco/CredibanCo.java
@@ -1,7 +1,9 @@
 package com.grpc.credibanco;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CredibanCo {
@@ -9,6 +11,10 @@ public class CredibanCo {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(CredibanCo.class);
         springApplication.run(args);
+    }
 
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
--- grpc-payu-demo/visanet/src/main/java/com/grpc/visanet/Visanet.java
+++ grpc-payu-demo/visanet/src/main/java/com/grpc/visanet/Visanet.java
@@ -1,7 +1,9 @@
 package com.grpc.visanet;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Visanet {
@@ -9,6 +11,10 @@ public class Visanet {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Visanet.class);
         springApplication.run(args);
+    }
 
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
