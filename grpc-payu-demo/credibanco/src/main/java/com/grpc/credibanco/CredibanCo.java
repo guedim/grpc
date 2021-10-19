8e2c8d4e54569f66489ca83ba32ce072b603914b
Mario Guerrero <mario.guerrero@payulatam.com>
2021-10-19 04:19:29 -0500

    added traicing

--- grpc-payu-demo/credibanco/src/main/java/com/grpc/credibanco/CredibanCo.java
+++ grpc-payu-demo/credibanco/src/main/java/com/grpc/credibanco/CredibanCo.java
@@ -1,6 +1,9 @@
 package com.grpc.credibanco;
 
+import brave.Tracing;
+import brave.grpc.GrpcTracing;
 import brave.sampler.Sampler;
+import io.grpc.ClientInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +19,17 @@ public class CredibanCo {
     @Bean
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @Bean
+    public GrpcTracing grpcTracing(Tracing tracing) {
+        return GrpcTracing.create(tracing);
+    }
+
+    //We also create a client-side interceptor and put that in the context, this interceptor can then be injected into gRPC clients and
+    //then applied to the managed channel.
+    @Bean
+    ClientInterceptor grpcClientSleuthInterceptor(GrpcTracing grpcTracing) {
+        return grpcTracing.newClientInterceptor();
     }
 }
